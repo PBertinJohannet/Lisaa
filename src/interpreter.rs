3265ca05d61c0ae35ffda558edbd964af9bc1908
xahigoyez <pierre.bertin-johannet@orange.fr>
2018-02-19 02:33:29 +0100

    added binary operations

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,7 +1,7 @@
 //! The interpreter for the language
 use expression::{Expr, UnaryExpr, LiteralExpr, BinaryExpr};
 use token::TokenType;
-use operations::UnaryOperations;
+use operations::{BinaryOperations, UnaryOperations};
 
 /// The interpreter's struct, will interpret the expressions.
 pub struct Interpreter {
@@ -30,14 +30,25 @@ impl Interpreter {
     pub fn unary(&self, exp : &UnaryExpr) -> Result<LiteralExpr, String>{
         let exp_res = self.evaluate(exp.expression())?;
         match exp.operator().get_type() {
-            &TokenType::MINUS => exp_res.minus(),
+            &TokenType::MINUS => UnaryOperations::minus(&exp_res),
             &TokenType::BANG => exp_res.bang(),
             e => Err(format!("operator {:?} can not be aplied to one value", e))
         }
     }
     /// Evaluates a binary expression.
-    pub fn binary(&self, _exp : &BinaryExpr) -> Result<LiteralExpr, String>{
-        unimplemented!();
-        //let mut exp_res = self.evaluate(exp.expression());
+    pub fn binary(&self, exp : &BinaryExpr) -> Result<LiteralExpr, String>{
+        let exp_left = self.evaluate(exp.lhs())?;
+        let exp_right = self.evaluate(exp.rhs())?;
+        match exp.operator().get_type() {
+            &TokenType::MINUS => BinaryOperations::minus(&exp_left,&exp_right),
+            &TokenType::PLUS => exp_left.plus(&exp_right),
+            &TokenType::STAR => exp_left.times(&exp_right),
+            &TokenType::SLASH => exp_left.divide(&exp_right),
+            &TokenType::GreaterEqual => exp_left.ge(&exp_right),
+            &TokenType::GREATER => exp_left.gt(&exp_right),
+            &TokenType::LessEqual => exp_left.le(&exp_right),
+            &TokenType::LESS => exp_left.lt(&exp_right),
+            e => Err(format!("operator {:?} can not be applied to binbary expression", e))
+        }
     }
 }
--- src/operations.rs
+++ src/operations.rs
@@ -25,3 +25,107 @@ impl UnaryOperations for LiteralExpr {
         }
     }
 }
+
+/// The trait for binary operations, lets go for a lot of matches.
+/// No implicit conversions atm.
+pub trait BinaryOperations {
+    /// Substract other from self.
+    fn minus(&self, other : &Self) -> Result<LiteralExpr, String>;
+    /// Add other to self
+    fn plus(&self, other : &Self) -> Result<LiteralExpr, String>;
+    /// Multiply other and self
+    fn times(&self, other : &Self) -> Result<LiteralExpr, String>;
+    /// Divide self by other.
+    fn divide(&self, other : &Self) -> Result<LiteralExpr, String>;
+    /// Returns 1 or 0 if other is lower
+    fn ge(&self, other : &Self) -> Result<LiteralExpr, String>;
+    /// Returns 1 or 0 if other is lower or equal
+    fn gt(&self, other : &Self) -> Result<LiteralExpr, String>;
+    /// Returns 1 or 0 if other is greater
+    fn le(&self, other : &Self) -> Result<LiteralExpr, String>;
+    /// Returns 1 or 0 if other is greater or equal.
+    fn lt(&self, other : &Self) -> Result<LiteralExpr, String>;
+}
+
+
+impl BinaryOperations for LiteralExpr {
+    fn minus(&self, other: &Self) -> Result<LiteralExpr, String> {
+        match self{
+            &LiteralExpr::NUMBER(n) =>  match other {
+                &LiteralExpr::NUMBER(k) => Ok(LiteralExpr::NUMBER(n - k)),
+                _ => Err("Cannot substract string fom int".to_string()),
+            }
+            _ => Err("Cannot substract fom string".to_string()),
+        }
+    }
+
+    fn plus(&self, other: &Self) -> Result<LiteralExpr, String> {
+        match self{
+            &LiteralExpr::NUMBER(n) =>  match other {
+                &LiteralExpr::NUMBER(k) => Ok(LiteralExpr::NUMBER(n + k)),
+                _ => Err("Cannot add string to int".to_string()),
+            }
+            _ => Err("Cannot add to string".to_string()),
+        }
+    }
+
+    fn times(&self, other: &Self) -> Result<LiteralExpr, String> {
+        match self{
+            &LiteralExpr::NUMBER(n) =>  match other {
+                &LiteralExpr::NUMBER(k) => Ok(LiteralExpr::NUMBER(n + k)),
+                _ => Err("Cannot multiply string with int".to_string()),
+            }
+            _ => Err("Cannot multiply with string".to_string()),
+        }
+    }
+
+    fn divide(&self, other: &Self) -> Result<LiteralExpr, String> {
+        match self{
+            &LiteralExpr::NUMBER(n) =>  match other {
+                &LiteralExpr::NUMBER(k) => Ok(LiteralExpr::NUMBER(n / k)),
+                _ => Err("Cannot divide string with int".to_string()),
+            }
+            _ => Err("Cannot divide with string".to_string()),
+        }
+    }
+
+    fn ge(&self, other: &Self) -> Result<LiteralExpr, String> {
+        match self{
+            &LiteralExpr::NUMBER(n) =>  match other {
+                &LiteralExpr::NUMBER(k) => Ok(LiteralExpr::NUMBER((n >= k) as i32 as f64)),
+                _ => Err("Cannot compare string with int".to_string()),
+            }
+            _ => Err("Cannot compare with string".to_string()),
+        }
+    }
+
+    fn gt(&self, other: &Self) -> Result<LiteralExpr, String> {
+        match self{
+            &LiteralExpr::NUMBER(n) =>  match other {
+                &LiteralExpr::NUMBER(k) => Ok(LiteralExpr::NUMBER((n > k) as i32 as f64)),
+                _ => Err("Cannot compare string with int".to_string()),
+            }
+            _ => Err("Cannot compare with string".to_string()),
+        }
+    }
+
+    fn le(&self, other: &Self) -> Result<LiteralExpr, String> {
+        match self{
+            &LiteralExpr::NUMBER(n) =>  match other {
+                &LiteralExpr::NUMBER(k) => Ok(LiteralExpr::NUMBER((n <= k) as i32 as f64)),
+                _ => Err("Cannot compare string with int".to_string()),
+            }
+            _ => Err("Cannot compare with string".to_string()),
+        }
+    }
+
+    fn lt(&self, other: &Self) -> Result<LiteralExpr, String> {
+        match self{
+            &LiteralExpr::NUMBER(n) =>  match other {
+                &LiteralExpr::NUMBER(k) => Ok(LiteralExpr::NUMBER((n < k) as i32 as f64)),
+                _ => Err("Cannot compare string with int".to_string()),
+            }
+            _ => Err("Cannot compare with string".to_string()),
+        }
+    }
+}
